45048a5ecff40af1842c74f4bcab3614900d330a
LucasLeitche <llneves019@gmail.com>
2024-06-07 17:57:41 -0300

    :rocket: Building project by yaml

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,7 +3,5 @@
 import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
 
 export default function Resume() {
-  return <main className="container">
-    <Resume></Resume>
-  </main>;
+  return <main className="container"></main>;
 }
