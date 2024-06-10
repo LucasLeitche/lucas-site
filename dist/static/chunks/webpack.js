a6768711d4378a684323d00c05760051bf1ee211
LucasLeitche <llneves019@gmail.com>
2024-06-10 12:40:35 -0300

    set chache pipeline config

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1b5bde62161c03f8"; }
+/******/ 		__webpack_require__.h = function() { return "a6d5b388b0ec8d65"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
